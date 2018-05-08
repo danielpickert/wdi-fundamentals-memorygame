c8d0d4dd9ee09e622b3e10723b97ee6cda55cfd5
danielpickert <38669130+danielpickert@users.noreply.github.com>
2018-05-08 14:51:23 -0400

    Add files via upload
    I had trouble throughout the units committing each stage of my memory game to the GitHub repository. It wasn't working. So I just manually added the whole file at the end once I finished.

---
+++ memory_game/js/main.js
@@ -0,0 +1,77 @@
+console.log("Up and running!");
+
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}];
+
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
+}
+
+var flipCard = function () {
+cardId = this.getAttribute('data-id');
+console.log (cardId);
+
+cardsInPlay.push(cards[cardId].card);
+
+this.setAttribute('src',cards[cardId].cardImage);
+if (cardsInPlay.length===2) {
+	checkforMatch();
+	} else { 
+	cardsInPlay = [];
+}
+
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+}
+
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+var cardElement = document.createElement('img');
+cardElement.setAttribute('src', 'images/back.png');
+cardElement.setAttribute('data-id', i);
+cardElement.addEventListener('click', flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
